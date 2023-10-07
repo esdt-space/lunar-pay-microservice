7308cd0f6afcf56995d70ca6b4ba0c4124ca0a2d
Alex <alex@devaccent.com>
2023-10-08 02:11:44 +0300

    Fix caching library issues with upstash Redis

--- src/libs/caching/caching-config.service.ts
+++ src/libs/caching/caching-config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ioRedisStore } from '@tirke/node-cache-manager-ioredis';
+import { redisStore } from 'cache-manager-ioredis-yet';
 import {
   CacheModuleOptions,
   CacheOptionsFactory,
@@ -14,12 +14,13 @@ export class CacheConfigService implements CacheOptionsFactory {
   async createCacheOptions(): Promise<CacheModuleOptions> {
     return {
       ttl: 5,
-      store: ioRedisStore({
+      store: (await redisStore({
         host: this.config.get<string>('REDIS_HOST'),
         port: this.config.get<number>('REDIS_PORT'),
         username: this.config.get<string>('REDIS_USERNAME'),
         password: this.config.get<string>('REDIS_PASSWORD'),
-      }) as unknown as CacheStore,
+        family: 6,
+      })) as unknown as CacheStore,
     };
   }
 }
