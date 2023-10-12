5f6b60ee0a28da0a67ebfff0ba7eefa87d0d32d6
Alex <alex@devaccent.com>
2023-10-12 03:51:11 +0300

    Add @ApiProperty decorator to pagination params

--- src/common/models/pagination.params.model.ts
+++ src/common/models/pagination.params.model.ts
@@ -1,13 +1,16 @@
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, Min, IsOptional, Max } from 'class-validator';
 
 export default class PaginationParams {
+  @ApiProperty({ required: false })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(0)
   skip?: number = 0;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
