c20854a9ee0885c0247f45f5cfde6b163e7116e7
David Banciu <davidbanciu57@gmail.com>
2023-11-10 10:26:02 +0000

    Created a dto for signed agreements and updated the get signed agreements functionality to only return the values inside the dto

---
+++ src/features/payment-agreements/dto/get-signed-agreement.dto.ts
@@ -0,0 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsArray, IsOptional, IsNumber, IsDateString } from 'class-validator';
+
+export class GetSignedAgreementDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  ownerName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  itemName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  tokenIdentifier: string;
+
+  @IsArray()
+  @ApiProperty()
+  benefits: string[];
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  content?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  description?: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  frequency: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  accountsCount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  fixedAmount: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  @ApiProperty()
+  createdAt: Date;
+}
--- src/features/payment-agreements/payment-agreement-members.service.ts
+++ src/features/payment-agreements/payment-agreement-members.service.ts
@@ -5,6 +5,7 @@ import { CreateAgreementMemberDto } from './dto';
 import { PaymentAgreement } from './payment-agreement.schema';
 import { PaymentAgreementMemberRepository } from './payment-agreement-member.repository';
 import { PaymentAgreementRepository } from './payment-agreement.repository';
+import { GetSignedAgreementDto } from './dto/get-signed-agreement.dto';
 
 @Injectable()
 export class PaymentAgreementMembersService {
@@ -13,11 +14,27 @@ export class PaymentAgreementMembersService {
     private readonly agreementsRepository: PaymentAgreementRepository,
   ) {}
 
-  async findAddressMemberships(address: string): Promise<PaymentAgreement[]> {
+  async findAddressMemberships(address: string): Promise<GetSignedAgreementDto[]> {
     const memberships = await this.repository.model.find({ member: address });
 
     const aggrementIds = memberships.map(item => item.internalAgreementId);
-    return this.agreementsRepository.find({ _id: { $in: aggrementIds } });
+
+    const allSignedAgreements: PaymentAgreement[] = await this.agreementsRepository.find({ _id: { $in: aggrementIds } });
+
+    return allSignedAgreements.map((el) => {
+      return {
+        ownerName: el.ownerName,
+        itemName: el.itemName,
+        tokenIdentifier: el.tokenIdentifier,
+        benefits: el.benefits,
+        content: el.content,
+        description: el.description,
+        frequency: el.frequency,
+        accountsCount: el.accountsCount,
+        fixedAmount: el.fixedAmount,
+        createdAt: el.createdAt
+      }
+    })
   }
 
   async findAgreementMembers(id: Types.ObjectId): Promise<PaymentAgreement[]> {
