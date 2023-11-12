8c8cee37c90c50cf4428b823b106117da1c095ac
David Banciu <davidbanciu57@gmail.com>
2023-11-12 20:43:20 +0000

    Decoding event: wip

--- src/events-notifier/events/payment-agreement/claim/trigger-agreement-event.topics.ts
+++ src/events-notifier/events/payment-agreement/claim/trigger-agreement-event.topics.ts
@@ -1,4 +1,7 @@
 import { LunarPayEventTopics } from '@/events-notifier/events/lunar-pay-event.topics';
+import { TransactionEvent, TransactionEventTopic } from '@multiversx/sdk-network-providers/out';
+import { ResultsParser } from '@multiversx/sdk-core/out';
+import abiRegistry from '../../../../common/protocol/abi/lunar-pay.abi.json';
 
 export class TriggerAgreementEventTopics extends LunarPayEventTopics {
   private readonly agreementId: number;
@@ -8,6 +11,23 @@ export class TriggerAgreementEventTopics extends LunarPayEventTopics {
 
   constructor(rawTopics: string[]) {
     super(rawTopics);
+
+    const parser = new ResultsParser();
+    // const eventDefinition = abiRegistry.events.filter((el) => el.identifier === 'triggerAgreement');
+
+    const event = new TransactionEvent({
+        identifier: "triggerAgreement",
+        topics: [
+            new TransactionEventTopic(rawTopics[1]),
+            new TransactionEventTopic(rawTopics[2]),
+            new TransactionEventTopic(rawTopics[3]),
+            new TransactionEventTopic(rawTopics[4]),
+        ],
+    });
+
+    const bundle = parser.parseUntypedOutcome(event as any)
+    // const bundle = parser.parseEvent(event, eventDefinition.pop());
+    console.log('logging out result',bundle)
 
     this.agreementId = this.parseIntValue(rawTopics[1]);
     this.accounts = Buffer.from(rawTopics[2], 'base64').toString().split(',')
