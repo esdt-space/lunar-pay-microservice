9048b6030de48f7523a283052f1e4ec0d40bc44e
Alex <alex@devaccent.com>
2024-01-19 18:52:24 +0200

    Add metadata to sign-payment-agreement event.

--- src/events-notifier/events/payment-agreement/sign/sign-payment-agreement-event.topics.ts
+++ src/events-notifier/events/payment-agreement/sign/sign-payment-agreement-event.topics.ts
@@ -5,6 +5,7 @@ export class SignPaymentAgreementEventTopics extends LunarPayEventTopics {
   private readonly address: Address;
   private readonly agreementId: number;
   private readonly signedAt: number;
+  private readonly metadata: string;
 
   constructor(rawTopics: string[]) {
     super(rawTopics);
@@ -12,14 +13,16 @@ export class SignPaymentAgreementEventTopics extends LunarPayEventTopics {
     this.agreementId = this.parseIntValue(rawTopics[1]);
     this.address = new Address(Buffer.from(rawTopics[2], 'base64'));
     this.signedAt = this.parseIntValue(rawTopics[3]);
+    this.metadata = 'metadata';
   }
 
   toPlainObject() {
     return {
-      eventName: this.eventName,
       agreementId: this.agreementId,
       address: this.address.bech32(),
       signedAt: new Date(this.signedAt * 1000),
+      metadata: this.metadata,
+      eventName: this.eventName,
     };
   }
 }
