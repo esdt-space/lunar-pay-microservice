cb8b1505f89351b13f7e5e73d7282c2189dd7448
Alex <alex@devaccent.com>
2024-01-19 05:10:45 +0200

    Make fields optional in UpdateAgreementDto
    The "ownerName", "itemName", "benefits", and "description" fields in the UpdateAgreementDto class have been made optional. This change was needed to allow more flexibility when updating payment agreements.

--- src/features/payment-agreements/dto/update-agreement.dto.ts
+++ src/features/payment-agreements/dto/update-agreement.dto.ts
@@ -3,16 +3,19 @@ import { IsString, IsNotEmpty, IsArray, IsOptional } from 'class-validator';
 
 export class UpdateAgreementDto {
   @IsString()
+  @IsOptional()
   @IsNotEmpty()
   @ApiProperty()
   ownerName: string;
 
   @IsString()
+  @IsOptional()
   @IsNotEmpty()
   @ApiProperty()
   itemName: string;
 
   @IsArray()
+  @IsOptional()
   @ApiProperty()
   benefits: string[];
 
@@ -22,6 +25,7 @@ export class UpdateAgreementDto {
   content: string;
 
   @IsString()
+  @IsOptional()
   @ApiProperty()
   description?: string;
 
