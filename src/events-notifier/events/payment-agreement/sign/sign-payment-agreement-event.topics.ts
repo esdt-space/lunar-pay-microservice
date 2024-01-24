af5ebd5b9334f62620ed6c69d82ca833cf764903
David Banciu <davidbanciu57@gmail.com>
2024-01-24 15:58:04 +0000

    Refactored the decoding of sign payment agreement

--- src/events-notifier/events/payment-agreement/sign/sign-payment-agreement-event.topics.ts
+++ src/events-notifier/events/payment-agreement/sign/sign-payment-agreement-event.topics.ts
@@ -1,5 +1,16 @@
-import { Address } from '@multiversx/sdk-core/out';
+import BigNumber from 'bignumber.js';
+import { AbiRegistry, Address, ResultsParser } from '@multiversx/sdk-core/out';
+import { TransactionEvent, TransactionEventTopic } from '@multiversx/sdk-network-providers/out';
+
+import abi from '@/common/protocol/abi/lunarpay.abi.json';
 import { LunarPayEventTopics } from '@/events-notifier/events/lunar-pay-event.topics';
+
+type ParseResult = {
+  agreement_id: BigNumber,
+  member: Address,
+  signed_at: BigNumber,
+  metadata: string,
+}
 
 export class SignPaymentAgreementEventTopics extends LunarPayEventTopics {
   private readonly address: Address;
@@ -10,10 +21,27 @@ export class SignPaymentAgreementEventTopics extends LunarPayEventTopics {
   constructor(rawTopics: string[]) {
     super(rawTopics);
 
-    this.agreementId = this.parseIntValue(rawTopics[1]);
-    this.address = new Address(Buffer.from(rawTopics[2], 'base64'));
-    this.signedAt = this.parseIntValue(rawTopics[3]);
-    this.metadata = 'metadata';
+    const parser = new ResultsParser();
+    const abiRegistry = AbiRegistry.create(abi);
+    const eventDefinition = abiRegistry.getEvent(this.eventName);
+
+    const event = new TransactionEvent({
+      identifier: 'signPaymentAgreement',
+      topics: [
+        new TransactionEventTopic(rawTopics[0]),
+        new TransactionEventTopic(rawTopics[1]),
+        new TransactionEventTopic(rawTopics[2]),
+        new TransactionEventTopic(rawTopics[3]),
+        new TransactionEventTopic(rawTopics[4]),
+      ],
+    });
+
+    const bundle = parser.parseEvent(event, eventDefinition) as ParseResult;
+
+    this.agreementId = bundle.agreement_id.toNumber();
+    this.address = new Address(bundle.member);
+    this.signedAt = bundle.signed_at.toNumber();
+    this.metadata = bundle.metadata;
   }
 
   toPlainObject() {
