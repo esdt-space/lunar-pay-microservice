d3cd933943830d9eaa95387d80b9fc515913df29
Alexandru Berce <alex@devaccent.com>
2024-03-19 17:16:25 +0200

    Add postgres config

--- src/libs/database/database.providers.module.ts
+++ src/libs/database/database.providers.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongoDatabaseModule } from './mongo';
+import { PostgresDatabaseModule } from './postgres/postgres.database.module';
 
 @Module({
-  imports: [MongoDatabaseModule],
-  exports: [MongoDatabaseModule],
+  imports: [MongoDatabaseModule, PostgresDatabaseModule],
+  exports: [MongoDatabaseModule, PostgresDatabaseModule],
 })
 export class DatabaseProvidersModule {}
---
+++ src/libs/database/postgres/postgres.database.module.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from "@nestjs/config";
+import { Global, Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+
+        logging: ['error', 'migration', 'schema'],
+        synchronize: true,
+        autoLoadEntities: true,
+      })
+    }),
+  ],
+})
+export class PostgresDatabaseModule {}
