cac115d70caf4287af3a35303ada7a164b5a940b
David Banciu <davidbanciu57@gmail.com>
2024-04-08 09:17:22 +0100

    Updated DepositWithdrawEventTopics to use multiversx library for decoding

--- src/events-notifier/events/token-management/topics/deposit-withdraw-event.topics.ts
+++ src/events-notifier/events/token-management/topics/deposit-withdraw-event.topics.ts
@@ -1,22 +1,43 @@
 import BigNumber from 'bignumber.js';
-import { Address } from '@multiversx/sdk-core/out';
+import { AbiRegistry, Address, ResultsParser } from '@multiversx/sdk-core/out';
+import { TransactionEvent, TransactionEventTopic } from '@multiversx/sdk-network-providers/out';
 
+import abi from '@/common/protocol/abi/lunarpay.abi.json';
 import { GenericToken } from '@/libs/blockchain/mvx/event-decoder';
 import { LunarPayEventTopics } from '../../lunar-pay-event.topics';
+import { DepositWithdrawParseEvent } from '../types';
 
 export class DepositWithdrawEventTopics extends LunarPayEventTopics {
   private readonly address: Address;
   private readonly tokenIdentifier: string;
   private readonly tokenNonce: string | number;
-  private readonly amount: number;
+  private readonly amount: string;
 
   constructor(rawTopics: string[]) {
     super(rawTopics);
 
-    this.address = new Address(Buffer.from(rawTopics[1], 'base64'));
-    this.tokenIdentifier = Buffer.from(rawTopics[2], 'base64').toString();
-    this.tokenNonce = this.parseIntValue(rawTopics[3]);
-    this.amount = this.parseIntValue(rawTopics[4]);
+    const parser = new ResultsParser();
+    const abiRegistry = AbiRegistry.create(abi);
+    const eventDefinition = abiRegistry.getEvent(this.eventName);
+
+    const event = new TransactionEvent({
+      identifier: this.eventName,
+      topics: [
+        new TransactionEventTopic(rawTopics[0]),
+        new TransactionEventTopic(rawTopics[1]),
+        new TransactionEventTopic(rawTopics[2]),
+        new TransactionEventTopic(rawTopics[3]),
+        new TransactionEventTopic(rawTopics[4]),
+        new TransactionEventTopic(rawTopics[5]),
+      ],
+    });
+
+    const parsedEvent = parser.parseEvent(event, eventDefinition) as DepositWithdrawParseEvent
+
+    this.address = new Address(parsedEvent.address);
+    this.tokenIdentifier = parsedEvent.token_identifier.toString();
+    this.tokenNonce = parsedEvent.token_nonce.toNumber();
+    this.amount = parsedEvent.amount.toString();
   }
 
   toPlainObject() {
--- src/events-notifier/events/token-management/types.ts
+++ src/events-notifier/events/token-management/types.ts
@@ -1,4 +1,6 @@
 import { GenericToken } from "@/libs/blockchain/mvx/event-decoder";
+import { Address } from "@multiversx/sdk-core/out";
+import BigNumber from "bignumber.js";
 
 export type TransferParsedEventResult = {
   sender: string;
@@ -6,6 +8,13 @@ export type TransferParsedEventResult = {
   eventName: string;
   isInternal: boolean;
   token: GenericToken;
+}
+
+export type DepositWithdrawParseEvent = {
+  address: Address,
+  token_identifier: string,
+  token_nonce: BigNumber,
+  amount: string,
 }
 
 export type DepositWithdrawParsedEventResult = {
